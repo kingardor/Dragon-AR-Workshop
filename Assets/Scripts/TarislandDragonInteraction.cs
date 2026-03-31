75451ca71b87525553d1d41527a9fd9a4afee550
Akash James <kn1ght@Akashs-MacBook-Air.local>
2026-03-31 07:15:57 +0530

    feat: add TarislandDragonInteraction and TarislandDragonSetup scripts

---
+++ Assets/Scripts/TarislandDragonInteraction.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(XRGrabInteractable))]
+public class TarislandDragonInteraction : MonoBehaviour
+{
+    [SerializeField] float holdToMoveDelay = 0.35f;
+
+    // Populate in the Inspector with the exact Animator state names (Task 6, Step 5).
+    // Index 0 = idle/rest (played by Animator Controller default, NOT by tap).
+    // Index 1+ = action clips cycled on each tap.
+    // Example: { "Idle", "Fly", "Attack", "Death" }
+    [SerializeField] string[] animationStates;
+
+    Animator animator;
+    XRGrabInteractable grabInteractable;
+
+    int currentAnimIndex;
+    bool isSelected;
+    bool movedDuringHold;
+    float selectTime;
+    Vector3 savedPosition;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+    }
+
+    void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(OnSelectEntered);
+        grabInteractable.selectExited.AddListener(OnSelectExited);
+    }
+
+    void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnSelectEntered);
+        grabInteractable.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    void OnDestroy()
+    {
+        if (Application.isPlaying)
+            DragonEvents.NotifyDestroyed();
+    }
+
+    void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        isSelected = true;
+        movedDuringHold = false;
+        selectTime = Time.time;
+        savedPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (!isSelected) return;
+
+        if (Time.time - selectTime < holdToMoveDelay)
+            transform.position = savedPosition;
+        else if (!movedDuringHold)
+            movedDuringHold = true;
+    }
+
+    void OnSelectExited(SelectExitEventArgs args)
+    {
+        isSelected = false;
+
+        if (!movedDuringHold && animationStates != null && animationStates.Length > 0)
+        {
+            currentAnimIndex = (currentAnimIndex + 1) % animationStates.Length;
+            animator.Play(animationStates[currentAnimIndex]);
+        }
+    }
+}
